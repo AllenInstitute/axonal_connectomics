54a9f346356dd6857223d08445381c56872d7e90
Shubha Bhaskaran <shubha.bhaskaran@osxltKYMD6M.local>
2021-05-17 15:20:23 -0700

    cleanup functionality

--- acpreprocessing/header_parse.py
+++ acpreprocessing/header_parse.py
@@ -6,42 +6,52 @@ This is a temporary script file.
 """
 
 import json
-from PIL import Image, ImageSequence
+from PIL import Image
 import sys
 import os
 from utils import io
 
 
+def parse_cropped(inf, head):
+    head['pixelResolution'] = [float(inf[2]),float(inf[2]),float(inf[2])]
+    head['type'] = inf[3]
+    
+def parse_uncropped(res, head):
+    head['pixelResolution'] = [float(res['PixelSizeUm']),float(res['PixelSizeUm']),float(res['PixelSizeUm'])]
+    head['type'] = res['PixelType']
+    
+
 def main():
-     cwd = os.getcwd()
      args = sys.argv[1:]
-     if len(args) == 2 and args[0] == '-o':
-         fo = args[1]
+     if len(args) == 5 and args[1] == '-i' and args[3] == '-o':
+         input_dir = args[2]
+         fo = args[4]
+         cropped = args[0]
          data = []
          k = 0
-         direct = os.listdir(cwd)
+         direct = os.listdir(input_dir)
          direct.sort()
-         for tiff in direct:
-             if tiff.endswith(".tif") and tiff.startswith("cropmore"):
-                 print("processing "+tiff)
-                 img = Image.open(tiff)
+         for file in direct:
+             if file.endswith(".tif"):
+                 print("parsing "+ file)
+                 img = Image.open(input_dir + file)
                  si = img.size
                  meta_dict = {str(key) : img.tag[key] for key in img.tag.keys()}
                  # print(meta_dict)
-                 inf = meta_dict['270'][0].split(';')
                  head = {"index":[],"file":[],"position":[],"size":[],"pixelResolution":[],"type":[]}
                  head['index'] = k
-                 head['file'] = cwd+"/"+tiff
-                 #change this when we get more information
+                 head['file'] = input_dir+"/"+file
+                 #TODO change this when we get more information
                  head['position'] = [0,0-(400*k),0]
                  head['size'] = [si[0],si[1],img.n_frames]
                  #if cropped tiff
-                 head['pixelResolution'] = [float(inf[2]),float(inf[2]),float(inf[2])]
-                 head['type'] = inf[3]
-                 #if uncropped original tiff
+                 if cropped == "True":
+                     parse_cropped(meta_dict['270'][0].split(';'),head)
+                 else:
+                     parse_uncropped(json.loads(meta_dict['51123'][0]), head)
                  data.append(head)
                  k=k+1
-         io.save_metadata(fo+".json", data)
+         io.save_metadata(input_dir+"/"+fo+".json", data)
          
      else:
          sys.exit("Input Argument Error")
