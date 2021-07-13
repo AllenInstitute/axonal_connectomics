8e13b307f51d45ebd8e473d042be38067c23a3e7
Shubha Bhaskaran <shubha.bhaskaran@osxltKYMD6M.local>
2021-07-13 13:00:15 -0700

    updated pixeloverlap caluclation

--- acpreprocessing/header_parse.py
+++ acpreprocessing/header_parse.py
@@ -22,8 +22,32 @@ def parse_cropped(inf, head):
 def parse_uncropped(res, head):
     head['pixelResolution'] = [float(res['PixelSizeUm']),float(res['PixelSizeUm']),float(res['PixelSizeUm'])]
     head['type'] = res['PixelType']
-    
 
+def get_pixel_overlap(direct, input_dir):
+    #get w adjacent tiff stacks' headers
+    for file in direct:
+        if file.endswith(".tif"):
+            prefix = os.path.splitext(os.path.splitext(file)[0])[0][0:-1]
+            break
+    files = [input_dir + prefix + "1.ome.tif",input_dir + prefix + "2.ome.tif"]
+    it = 0
+    for f in files:
+        img = Image.open(f)
+        if it == 0:
+            md0 = {str(key) : img.tag[key] for key in img.tag.keys()}
+            header0 = json.loads(md0['51123'][0])
+        if it == 1:
+            md1 = {str(key) : img.tag[key] for key in img.tag.keys()}
+            header1 = json.loads(md1['51123'][0])
+        it = it+1
+        
+    #Calculate the pixel overlap
+    diff = abs(float(header1['XYStage:XY:31-ScanSlowAxisStopPosition(mm)'])-float(header0['XYStage:XY:31-ScanSlowAxisStopPosition(mm)']))
+    FOV = header1['PixelSizeUm']*header1["Height"]
+    pixeloverlap = (FOV-(diff*1000))/header1['PixelSizeUm']
+    print("PixelOverlap: "+ pixeloverlap)
+    return pixeloverlap
+    
 def main():
      args = sys.argv[1:]
      if len(args) == 6 and args[0] == '-crop' and args[2] == '-i' and args[4] == '-o':
@@ -34,10 +58,8 @@ def main():
          k = 0
          direct = os.listdir(input_dir)
          direct.sort()
-         #TODO fix magic numebrs
-         #FOV in um
-         FOV = 2048*6.5/25
-         pixeloverlap = 510
+         pixeloverlap = get_pixel_overlap(direct, input_dir)
+         
          for file in direct:
              if file.endswith(".tif"):
                  print("parsing "+ file)
@@ -50,7 +72,7 @@ def main():
                  #TODO: fix filepath to be on s3 instead of local if running on emr
                  head['file'] = input_dir+"/"+file
                  #TODO change this when we get more information
-                 head['position'] = [0,0-(pixeloverlap*k),0]
+                 head['position'] = [0,0+(pixeloverlap*k),0]
                  head['size'] = [si[0],si[1],img.n_frames]
                  #if cropped tiff
                  if cropped == "True":
