0366a3c9c8dc96faef33b7e5ce6e821f53c19550
shbhas <shubhabhas@gmail.com>
2021-11-08 05:31:52 -0800

    add run function

--- acpreprocessing/stitching_modules/nglink/create_nglink.py
+++ acpreprocessing/stitching_modules/nglink/create_nglink.py
@@ -6,29 +6,31 @@ from argschema.fields import NumpyArray, Boolean,Float, Int, Str
 import numpy as np
 import argschema
 
-class createNglinkSchema(argschema.ArgSchema):
-    Position = argschema.fields.Int(default=0, description='acquisition strip position number')
-    output_root = argschema.fields.String(default='', description='output root directory')
-    pixelResolution = NumpyArray(dtype=np.float, required=True,description='Pixel Resolution in um')
-    overlap = argschema.fields.Float(required=True, description='overlap between position strips')
-    ds_name = argschema.fields.String(default='', description='dataset name')
-
-if __name__ == '__main__':
-
-    # this defines a default dictionary that will be used if input_json is not specified
-    example_input = {
-            "output_root": "/ACdata/processed/testnglink/n5/",
-            "Position": 0,
+example_input = {
+            "outputRoot": "/ACdata/processed/testnglink/n5/",
+            "position": 0,
             "pixelResolution": [0.26, 0.26, 1],
             "overlap": 509.53846153846166,
-            'ds_name':'testnglink'
+            'dsName':'testnglink'
     }
-    # here is my ArgSchemaParser that processes my inputs
-    mod = ArgSchemaParser(input_data=example_input,
-                          schema_type=createNglinkSchema)
 
-    state = {"layers": []}
+class CreateNglinkSchema(argschema.ArgSchema):
+    position = argschema.fields.Int(default=0, description='acquisition strip position number')
+    outputRoot = argschema.fields.String(default='', description='output root directory')
+    pixelResolution = NumpyArray(dtype=float, required=True,description='Pixel Resolution in um')
+    overlap = argschema.fields.Float(required=True, description='overlap between position strips')
+    dsName = argschema.fields.String(default='', description='dataset name')
 
-    layer0 = create_state.create_layer(mod.args['output_root'], mod.args['Position'],mod.args['overlap'], mod.args['pixelResolution'])
-    create_state.add_layer(state, layer0)
-    write_nglink.write_url(mod.args['ds_name'], state)
+class Nglink():
+    def run(self):
+        mod = ArgSchemaParser(input_data=example_input,schema_type=CreateNglinkSchema)
+        state = {"layers": []}
+
+        layer0 = create_state.create_layer(mod.args['outputRoot'], mod.args['position'],mod.args['overlap'], mod.args['pixelResolution'])
+        create_state.add_layer(state, layer0)
+        write_nglink.write_url(mod.args['dsName'], state)
+
+
+if __name__ == '__main__':
+    mod = Nglink()
+    mod.run()
