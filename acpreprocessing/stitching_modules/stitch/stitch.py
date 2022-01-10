ff1c802693675aa22f7fab3d6b928d39fee1188e
shbhas <shubhabhas@gmail.com>
2022-01-10 09:34:05 -0800

    subprocess

--- acpreprocessing/stitching_modules/stitch/stitch.py
+++ acpreprocessing/stitching_modules/stitch/stitch.py
@@ -1,21 +1,27 @@
 import os
 from argschema import ArgSchemaParser, ArgSchema
-from argschema.fields import NumpyArray, Boolean,Float, Int, Str
+from argschema.fields import Str
 from acpreprocessing.stitching_modules.stitch import create_json
+import subprocess
 
 def stitch(stitchjson):
     curdir = os.getcwd()
-    os.chdir('/ACdata/stitching-spark/startup-scripts/')
-    os.system('python spark-local/stitch.py -i %s'%(stitchjson))
+    #os.chdir('/ACdata/stitching-spark/startup-scripts/')
+    subprocess.call(["python", "/ACdata/stitching-spark/startup-scripts/spark-local/stitch.py","-i",stitchjson])
+    #os.system('python spark-local/stitch.py -i %s'%(stitchjson))
     os.chdir(curdir)
     print("Finished stitching")
 
-class Stitch():
-    def run(self,stitchjson):
-        stitch(stitchjson)
+class StitchSchema(ArgSchema):
+    stitchjson = Str(required=True, description='inputjson for stitching')
+
+class Stitch(ArgSchemaParser):
+    default_schema = StitchSchema
+    
+    def run(self):
+        stitch(self.args['stitchjson'])
 
 if __name__ == '__main__':
-    mod = Stitch()
     outputDir = "/ACdata/processed/demoModules/output/"
     stitchjson = outputDir + "stitch.json"
-    mod.run(stitchjson)
+    Stitch.run(stitchjson)
