0998de0f2eb0b341873d4ec5e36db220995a4c09
shbhas <shubhabhas@gmail.com>
2022-01-28 13:18:56 -0800

    fix stitching bug

--- examples/runmodules.py
+++ examples/runmodules.py
@@ -10,97 +10,105 @@ from acpreprocessing.stitching_modules.stitch import create_json, stitch
 
 start = time.time()
 
-outputdir = "/ACdata/processed/MN7_RH_3_b5_S16_1_high_res_region"
-rootdir = "/ispim2_data/MN7_RH_3_b5_S16_1_high_res_region/"
-ds_name = "high_res_region"
+outputdir = "/ACdata/processed/MN7_RH_3_2_S30_1_high_res/"
+rootdir = "/ispim2_data/MN7_RH_3_2_S30_1_high_res/"
+ds_name = "high_res"
 
 md_input = {
-        "rootDir":rootdir,
-        "fname":"acqinfo_metadata.json"
+        "rootDir": rootdir,
+        "fname": "acqinfo_metadata.json"
         }
 metadata = parse_metadata.ParseMetadata(input_data=md_input)
 n_pos = metadata.get_number_of_positions()
+
 
 for pos in range(n_pos):
     convert_input = {
         "ds_name": f"pos{pos}",
         "max_mip": 4,
         "concurrency": 20,
-        "input_dir": f"{rootdir}{ds_name}_Pos{pos}",
-        "out_n5": f"{outputdir}Pos{pos}.n5"
+        "input_dir": f"{rootdir}/{ds_name}_Pos{pos}",
+        "out_n5": f"{outputdir}/Pos{pos}.n5"
         }
     multiscale_input = {
         "position": pos,
-        "outputDir": f"{outputdir}Pos{pos}.n5",
+        "outputDir": f"{outputdir}/Pos{pos}.n5",
         'max_mip':4,
         "rootDir":f"{rootdir}",
         "fname":"acqinfo_metadata.json"
         }
-        
-    #Convert to n5
-    #print(f"Converting from rootdir: {convert_input["input_dir"]}")
+       
+    # Convert to n5
+    # print(f"Converting from rootdir: {convert_input["input_dir"]}")
     mod = tiff_to_n5.TiffDirToN5(input_data=convert_input, args=[])
     mod.run()
 
-    #Add multiscale attributes
-    mod1 = multiscale.Multiscale(input_data = multiscale_input)
+    # Add multiscale attributes
+    mod1 = multiscale.Multiscale(input_data=multiscale_input)
     mod1.run()
 
-#Create overview nglink
-state = {"showDefaultAnnotations": False,"layers": []}
+# Create overview nglink
+state = {"showDefaultAnnotations": False, "layers": []}
 
-for pos in range (n_pos):
+for pos in range(n_pos):
     layer_input = {
         "position": pos,
         "outputDir": outputdir,
         "rootDir": rootdir
         }
-    mod2 = create_layer.NgLayer(input_data = layer_input)
+    mod2 = create_layer.NgLayer(input_data=layer_input)
     mod2.run(state)
 
-nglink_input={
+nglink_input = {
         "outputDir": outputdir,
         "fname": "nglink.txt"
         }
 create_nglink.Nglink(input_data=nglink_input).run(state)
 
 
+# Stitch
+mip_level = 3
 create_json_input = {
-        'rootDir':rootdir,
-        'outputDir':outputdir
+        'rootDir': rootdir,
+        'outputDir': outputdir,
+        "mip_level": mip_level
         }
-mod = create_json.CreateJson(input_data = create_json_input)
+mod = create_json.CreateJson(input_data=create_json_input)
 mod.run()
 
-stitchjson=os.path.join(outputdir,"stitch.json")
-stitch_input={
-        "stitchjson":stitchjson
+stitchjson = os.path.join(outputdir, "stitch.json")
+
+stitch_input = {
+        "stitchjson": stitchjson
         }
 mod = stitch.Stitch(input_data=stitch_input)
 mod.run()
 
-statejson = io.read_json(os.path.join(outputdir,"state.json"))
-#read stitch json in
-stitchoutjson = io.read_json(os.path.join(outputdir,"stitch-final.json"))
-    
-#update state json with stich coord
-for pos in range(0,n_pos-1):
+
+statejson = io.read_json(os.path.join(outputdir, "state.json"))
+# read stitch json in
+stitchoutjson = io.read_json(os.path.join(outputdir, "stitch-final.json"))
+
+# update state json with stich coord
+factor = 2**mip_level
+for pos in range(0, n_pos):
     try:
-        statejson['layers'][pos]['source']['transform']['matrix'][0][3]=stitchoutjson[pos]['position'][0]*4
-        statejson['layers'][pos]['source']['transform']['matrix'][1][3]=stitchoutjson[pos]['position'][1]*4
-        statejson['layers'][pos]['source']['transform']['matrix'][2][3]=stitchoutjson[pos]['position'][2]*4
-    except:
+        statejson['layers'][pos]['source']['transform']['matrix'][0][3] = stitchoutjson[pos]['position'][0]*factor
+        statejson['layers'][pos]['source']['transform']['matrix'][1][3] = stitchoutjson[pos]['position'][1]*factor
+        statejson['layers'][pos]['source']['transform']['matrix'][2][3] = stitchoutjson[pos]['position'][2]*factor
+    except IndexError:
         print("Something went wrong with the stitching output!")
 
-#create stitched nglink
-nglink_input={
+# create stitched nglink
+nglink_input = {
         "outputDir": outputdir,
         "fname": "stitched-nglink.txt"
         }
 create_nglink.Nglink(input_data=nglink_input).run(statejson)
 
-#save new state
-io.save_metadata(os.path.join(outputdir,"stitched-state.json"),statejson)
+# save new state
+io.save_metadata(os.path.join(outputdir, "stitched-state.json"), statejson)
 
-print('done testing')
+
+print('done processing')
 print(time.time()-start)
