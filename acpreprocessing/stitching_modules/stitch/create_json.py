c4eb0969cfcbb3d7bb7aa0b61e92b7555582e514
shbhas <shubhabhas@gmail.com>
2022-01-28 13:02:09 -0800

    fix stitching bug

--- acpreprocessing/stitching_modules/stitch/create_json.py
+++ acpreprocessing/stitching_modules/stitch/create_json.py
@@ -5,17 +5,20 @@ import json
 import argschema
 from acpreprocessing.utils import io
 import os
+import posixpath
 
 example_input = {
     "rootDir": "/ACdata/processed/demoModules/raw/",
     "outputDir": "/ACdata/processed/demoModules/output/",
+    "mip_level": 2
 }
 
 
-def get_pos_info(downdir, overlap, pos, pr, ind):
-    att = io.get_json(downdir+"attributes.json")
+def get_pos_info(downdir, overlap, pr, ind, mip_level):
+    factor = 2**mip_level
+    att = io.read_json(downdir+"attributes.json")
     sz = att["dimensions"]
-    yshift = overlap/4
+    yshift = sz[0]-overlap/factor
     if att['dataType'] == 'uint16':
         dtype = 'GRAY16'
     pos_info = {"file": downdir, "index": ind, "pixelResolution": pr,
@@ -26,6 +29,8 @@ def get_pos_info(downdir, overlap, pos, pr, ind):
 class CreateJsonSchema(argschema.ArgSchema):
     rootDir = Str(required=True, description='raw tiff root directory')
     outputDir = Str(required=True, description='output directory')
+    mip_level = Int(required=False, default=2,
+                    description='downsample level to perform stitching with')
 
 
 class CreateJson(argschema.ArgSchemaParser):
@@ -38,11 +43,16 @@ class CreateJson(argschema.ArgSchemaParser):
                 }
         md = parse_metadata.ParseMetadata(input_data=md_input)
         n_pos = md.get_number_of_positions()
-        for pos in range(0, n_pos-1):
-            downdir = self.args['outputDir']+f"/Pos{pos}.n5/"
-            "multirespos{pos}/s2/"
-            pos_info = get_pos_info(downdir, md.get_overlap(), pos,
-                                    md.get_pixel_resolution(), pos)
+
+        for pos in range(0, n_pos):
+            # downdir = self.args['outputDir'] + f"/Pos{pos}.n5/multirespos{pos}/s2/"
+            downdir = posixpath.join(
+                    self.args["outputDir"],
+                    f"Pos{pos}.n5/multirespos{pos}/s{self.args['mip_level']}/")
+            print(downdir)
+            pos_info = get_pos_info(downdir, md.get_overlap(),
+                                    md.get_pixel_resolution(), pos,
+                                    self.args['mip_level'])
             stitching_json.append(pos_info)
         fout = os.path.join(self.args['outputDir'], 'stitch.json')
         io.save_metadata(fout, stitching_json)
