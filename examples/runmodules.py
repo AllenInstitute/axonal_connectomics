7d1422321dacc8a74752cde0f48bd9b8099c56fc
shbhas <shubhabhas@gmail.com>
2022-01-29 07:55:28 -0800

    restructure runmodules so it can take json input

--- examples/runmodules.py
+++ examples/runmodules.py
@@ -5,110 +5,110 @@ from acpreprocessing.utils import io
 from acpreprocessing.stitching_modules.metadata import parse_metadata
 from acpreprocessing.stitching_modules.convert_to_n5 import tiff_to_n5
 from acpreprocessing.stitching_modules.multiscale_viewing  import multiscale
-from acpreprocessing.stitching_modules.nglink import create_layer, create_nglink
+from acpreprocessing.stitching_modules.nglink import create_layer, create_nglink, update_state
 from acpreprocessing.stitching_modules.stitch import create_json, stitch
+import argschema
+from argschema.fields import NumpyArray, Boolean, Float, Int, Str
 
 start = time.time()
 
-outputdir = "/ACdata/processed/MN7_RH_3_2_S30_1_high_res/"
-rootdir = "/ispim2_data/MN7_RH_3_2_S30_1_high_res/"
-ds_name = "high_res"
+run_input = {
+    "outputDir": "/ACdata/processed/MN7_RH_3_2_s27_high_res/",
+    "rootDir": "/ispim2_data/MN7_RH_3_2_s27_high_res/",
+    "ds_name": "ex1_1",
+    "mip_level": 3,
+    "md_filename": "acqinfo_metadata.json"
+}
 
-md_input = {
-        "rootDir": rootdir,
-        "fname": "acqinfo_metadata.json"
+
+class RunModulesSchema(argschema.ArgSchema):
+    outputDir = Str(required=True, description='output directory')
+    rootDir = Str(required=True, description='raw tiff root directory')
+    ds_name = Str(required=True)
+    mip_level = Int(required=False, default=3,
+                    description='downsample level to perform stitching with')
+    md_filename = Str(required=False, default="acqinfo_metadata.json",
+                      description='metadata file name')
+
+
+class RunModules(argschema.ArgSchemaParser):
+    default_schema = RunModulesSchema
+
+    def run(self):
+
+        md_input = {
+                "rootDir": run_input['rootDir'],
+                "fname": run_input['md_filename']
+                }
+        n_pos = parse_metadata.ParseMetadata(input_data=md_input).get_number_of_positions()
+        '''
+        for pos in range(n_pos):
+            convert_input = {
+                "ds_name": f"pos{pos}",
+                "max_mip": 4,
+                "concurrency": 20,
+                "input_dir": f"{run_input['rootDir']}/{run_input['ds_name']}_Pos{pos}",
+                "out_n5": f"{run_input['outputDir']}/Pos{pos}.n5"
+                }
+            multiscale_input = {
+                "position": pos,
+                "outputDir": f"{run_input['outputDir']}/Pos{pos}.n5",
+                'max_mip': 4,
+                "rootDir": f"{run_input['rootDir']}",
+                "fname": "acqinfo_metadata.json"
+                }
+
+            # Convert to n5
+            tiff_to_n5.TiffDirToN5(input_data=convert_input, args=[]).run()
+            # Add multiscale attributes
+            multiscale.Multiscale(input_data=multiscale_input).run()
+        '''
+        # Create overview nglink, initialize state
+        state = {"showDefaultAnnotations": False, "layers": []}
+
+        # Loop throuh each position to add a layer to state
+        for pos in range(n_pos):
+            layer_input = {
+                "position": pos,
+                "outputDir": run_input['outputDir'],
+                "rootDir": run_input['rootDir']
+                }
+            create_layer.NgLayer(input_data=layer_input).run(state)
+
+        # Create nglink from created state and estimated positions (overview link)
+        nglink_input = {
+                "outputDir": run_input['outputDir'],
+                "fname": "nglink.txt"
+                }
+        create_nglink.Nglink(input_data=nglink_input).run(state)
+
+        # Create Stitch.json which is input for stitching code
+        create_json_input = {
+                'rootDir': run_input['rootDir'],
+                'outputDir': run_input['outputDir'],
+                "mip_level": run_input['mip_level']
+                }
+        create_json.CreateJson(input_data=create_json_input).run()
+
+        # Run Stitching with stitch.json
+        stitchjsonpath = os.path.join(run_input['outputDir'], "stitch.json")
+        stitch_input = {
+                "stitchjson": stitchjsonpath
+                }
+        stitch.Stitch(input_data=stitch_input).run()
+
+        # update state json with stitched coord
+        update_state_input = {
+            'rootDir': run_input['rootDir'],
+            "outputDir": run_input['outputDir'],
+            'mip_level': run_input['mip_level'],
+            "fname": "stitched-nglink.txt"
         }
-metadata = parse_metadata.ParseMetadata(input_data=md_input)
-n_pos = metadata.get_number_of_positions()
+        update_state.UpdateState(input_data=update_state_input).run()
+
+        print('Done processing')
+        print(str((time.time()-start)/60) + " minutes elapsed")
 
 
-for pos in range(n_pos):
-    convert_input = {
-        "ds_name": f"pos{pos}",
-        "max_mip": 4,
-        "concurrency": 20,
-        "input_dir": f"{rootdir}/{ds_name}_Pos{pos}",
-        "out_n5": f"{outputdir}/Pos{pos}.n5"
-        }
-    multiscale_input = {
-        "position": pos,
-        "outputDir": f"{outputdir}/Pos{pos}.n5",
-        'max_mip':4,
-        "rootDir":f"{rootdir}",
-        "fname":"acqinfo_metadata.json"
-        }
-       
-    # Convert to n5
-    # print(f"Converting from rootdir: {convert_input["input_dir"]}")
-    mod = tiff_to_n5.TiffDirToN5(input_data=convert_input, args=[])
-    mod.run()
-
-    # Add multiscale attributes
-    mod1 = multiscale.Multiscale(input_data=multiscale_input)
-    mod1.run()
-
-# Create overview nglink
-state = {"showDefaultAnnotations": False, "layers": []}
-
-for pos in range(n_pos):
-    layer_input = {
-        "position": pos,
-        "outputDir": outputdir,
-        "rootDir": rootdir
-        }
-    mod2 = create_layer.NgLayer(input_data=layer_input)
-    mod2.run(state)
-
-nglink_input = {
-        "outputDir": outputdir,
-        "fname": "nglink.txt"
-        }
-create_nglink.Nglink(input_data=nglink_input).run(state)
-
-
-# Stitch
-mip_level = 3
-create_json_input = {
-        'rootDir': rootdir,
-        'outputDir': outputdir,
-        "mip_level": mip_level
-        }
-mod = create_json.CreateJson(input_data=create_json_input)
-mod.run()
-
-stitchjson = os.path.join(outputdir, "stitch.json")
-
-stitch_input = {
-        "stitchjson": stitchjson
-        }
-mod = stitch.Stitch(input_data=stitch_input)
-mod.run()
-
-
-statejson = io.read_json(os.path.join(outputdir, "state.json"))
-# read stitch json in
-stitchoutjson = io.read_json(os.path.join(outputdir, "stitch-final.json"))
-
-# update state json with stich coord
-factor = 2**mip_level
-for pos in range(0, n_pos):
-    try:
-        statejson['layers'][pos]['source']['transform']['matrix'][0][3] = stitchoutjson[pos]['position'][0]*factor
-        statejson['layers'][pos]['source']['transform']['matrix'][1][3] = stitchoutjson[pos]['position'][1]*factor
-        statejson['layers'][pos]['source']['transform']['matrix'][2][3] = stitchoutjson[pos]['position'][2]*factor
-    except IndexError:
-        print("Something went wrong with the stitching output!")
-
-# create stitched nglink
-nglink_input = {
-        "outputDir": outputdir,
-        "fname": "stitched-nglink.txt"
-        }
-create_nglink.Nglink(input_data=nglink_input).run(statejson)
-
-# save new state
-io.save_metadata(os.path.join(outputdir, "stitched-state.json"), statejson)
-
-
-print('done processing')
-print(time.time()-start)
+if __name__ == '__main__':
+    RunModules(run_input).run()
