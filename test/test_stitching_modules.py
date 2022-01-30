e9389bed755f82cb4fdd13b89f9d876155b887f3
shbhas <shubhabhas@gmail.com>
2022-01-29 17:46:22 -0800

    add some tests

--- test/test_stitching_modules.py
+++ test/test_stitching_modules.py
@@ -5,12 +5,58 @@ try:
 except ImportError:
     # FIXME: z5py installs with conda, not currently configured on ci
     pass
-import acpreprocessing.stitching_modules.metadata.parse_metadata
+from acpreprocessing.stitching_modules.metadata import parse_metadata
 import acpreprocessing.stitching_modules.multiscale_viewing.multiscale
 import acpreprocessing.stitching_modules.stitch.create_json
 import acpreprocessing.stitching_modules.stitch.stitch
-import acpreprocessing.stitching_modules.nglink.create_layer
+from acpreprocessing.stitching_modules.nglink import create_layer
+from acpreprocessing.stitching_modules.nglink import update_state
 import acpreprocessing.stitching_modules.nglink.create_nglink
 import acpreprocessing.stitching_modules.nglink.write_nglink
+from acpreprocessing.utils import io
+import os
+
+
+# Test ParseMetadata
+def test_parse_metadata():
+    mod = parse_metadata.ParseMetadata()
+    md = io.read_json(os.path.join("/ACdata/processed/demoModules/raw/",
+                                   "acqinfo_metadata.json"))
+    assert md == mod.get_md()
+    assert isinstance(md, dict)
+
+
+# Test multiscale - Not sure what tests can be done here without creating/deleting files
+# Test nglink.create_layer.create_layer
+@pytest.mark.parametrize("outputDir, position, overlap, pixelResolution",
+                         [("/testout", 2, 200, [0.1, 0.1, 0.1]),
+                          ("test2/testout/", 20, 500, [0.406, 0.406, 1.0])])
+def test_create_layer(outputDir, position, overlap, pixelResolution):
+    layer = create_layer.create_layer(outputDir, position,
+                                      overlap, pixelResolution)
+    url = "n5://http://bigkahuna.corp.alleninstitute.org"
+    url = os.path.join(url, outputDir + '/Pos%d.n5/multirespos%d' % (position, position))
+    assert layer["source"]["url"] == url
+    assert layer["source"]["transform"]["matrix"][1][3] == overlap*position
+    assert layer["source"]["transform"]["outputDimensions"]["x"][0] == pixelResolution[0]
+    assert layer["source"]["transform"]["outputDimensions"]["y"][0] == pixelResolution[1]
+    assert layer["source"]["transform"]["outputDimensions"]["z"][0] == pixelResolution[z]
+
+# Test update state
+@pytest.mark.parametrize("x, y, z, overlap, factor", [(30, 500, 2, 1000, 2)])
+def test_update_state(x, y, z, overlap, factor):
+    state = {}
+    state['layers'][0]['source']['transform']['matrix'][0][3] = 0
+    state['layers'][0]['source']['transform']['matrix'][1][3] = overlap
+    state['layers'][0]['source']['transform']['matrix'][2][3] = 0
+    stitchoutjson = {}
+    stitchoutjson[0]['position'][0] = x
+    stitchoutjson[0]['position'][1] = y
+    stitchoutjson[0]['position'][2] = z
+    update_state(state, stitchoutjson, 1, factor)
+    assert state['layers'][0]['source']['transform']['matrix'][0][3] == x*factor
+    assert state['layers'][0]['source']['transform']['matrix'][1][3] == y*factor
+    assert state['layers'][0]['source']['transform']['matrix'][2][3] == z*factor
+
 
 
