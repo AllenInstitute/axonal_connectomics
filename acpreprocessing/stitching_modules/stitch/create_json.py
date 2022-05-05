6c25393d3a7e58eba96dcea893b8cf9e789e5107
shbhas <shubhabhas@gmail.com>
2022-05-05 14:56:26 -0700

    updates to stitching such as deskew and reverse flags as well as connection to the db

--- acpreprocessing/stitching_modules/stitch/create_json.py
+++ acpreprocessing/stitching_modules/stitch/create_json.py
@@ -1,5 +1,5 @@
 from acpreprocessing.stitching_modules.metadata import parse_metadata
-from argschema.fields import Int, Str
+from argschema.fields import Int, Str, Boolean
 import argschema
 from acpreprocessing.utils import io
 import os
@@ -8,7 +8,8 @@ import posixpath
 example_input = {
     "rootDir": "/ACdata/processed/demoModules/raw/",
     "outputDir": "/ACdata/processed/demoModules/output/",
-    "mip_level": 2
+    "mip_level": 2,
+    "reverse": False
 }
 
 
@@ -17,19 +18,24 @@ class CreateJsonSchema(argschema.ArgSchema):
     outputDir = Str(required=True, description='output directory')
     mip_level = Int(required=False, default=2,
                     description='downsample level to perform stitching with')
-
+    reverse = Boolean(required=False, description='Whether position strips should be placed in reverse order')
+    dirname = Str(required = True, description="name of dataset folder")
 
 # Create specific position strip information needed for stitching
 # (including approximate coordinates using overlap)
-def get_pos_info(downdir, overlap, pr, ind, mip_level):
+def get_pos_info(downdir, overlap, pr, ind, mip_level, reverse):
     factor = 2**mip_level
     att = io.read_json(downdir+"attributes.json")
     sz = att["dimensions"]
     yshift = sz[0]-overlap/factor
     if att['dataType'] == 'uint16':
         dtype = 'GRAY16'
-    pos_info = {"file": downdir, "index": ind, "pixelResolution": pr,
-                "position": [0, ind*yshift, 0], "size": sz, "type": dtype}
+    if reverse:
+        pos_info = {"file": downdir, "index": ind, "pixelResolution": pr,
+                    "position": [0, -1*ind*yshift, 0], "size": sz, "type": dtype}
+    else:
+        pos_info = {"file": downdir, "index": ind, "pixelResolution": pr,
+                    "position": [0, ind*yshift, 0], "size": sz, "type": dtype}
     return pos_info
 
 
@@ -47,12 +53,12 @@ class CreateJson(argschema.ArgSchemaParser):
         for pos in range(0, n_pos):
             # downdir = self.args['outputDir'] + f"/Pos{pos}.n5/multirespos{pos}/s2/"
             downdir = posixpath.join(
-                    self.args["outputDir"],
-                    f"setup{pos}/timepoint0//s{self.args['mip_level']}/")
+                    self.args["outputDir"]+self.args["dirname"]+".n5/",
+                    f"setup{pos}/timepoint0/s{self.args['mip_level']}/")
             # print(downdir)
             pos_info = get_pos_info(downdir, md.get_overlap(),
                                     md.get_pixel_resolution(), pos,
-                                    self.args['mip_level'])
+                                    self.args['mip_level'],self.args["reverse"])
             stitching_json.append(pos_info)
 
         fout = os.path.join(self.args['outputDir'], 'stitch.json')
--- examples/runmodules.py
+++ examples/runmodules.py
@@ -1,24 +1,41 @@
 from pathlib import Path
 import time
 import os
+import numpy as np
 from acpreprocessing.utils import io
 from acpreprocessing.stitching_modules.metadata import parse_metadata
-from acpreprocessing.stitching_modules.convert_to_n5 import tiff_to_n5
+from acpreprocessing.stitching_modules.convert_to_n5 import acquisition_dir_to_n5_dir
 from acpreprocessing.stitching_modules.multiscale_viewing  import multiscale
 from acpreprocessing.stitching_modules.nglink import create_layer, create_nglink, update_state
 from acpreprocessing.stitching_modules.stitch import create_json, stitch
 import argschema
 from argschema.fields import NumpyArray, Boolean, Float, Int, Str
+from ddbclient import acquisition
 
 start = time.time()
 
+#TODO: make this more robust
+acq_client = acquisition.AcquisitionClient("http://bigkahuna.corp.alleninstitute.org/transfer_temp/api/api", subpath="")
+response_json = acq_client.query(
+    {
+        "filter": {"specimen_id": "MN6_2_a","session_id":"220427", 'section_num': '79'},
+        "projection": {"_id": False}
+    })
+#TODO: how to get index of correct data? overview, high_res, etc
+dir_ACdata=response_json[3]["data_location"]["ACTiff_dir_ACdata"]["uri"]
+rootDir = "/ACdata"+dir_ACdata.split(':')[1]
+scope = response_json[0]['scope']
+dirname = rootDir.split("/")[5]
+
 run_input = {
-    "outputDir": "/ACdata/processed/MN7_RH_3_2_S13_220307_high_res/",
-    "rootDir": "/ispim2_data/TBA_TO_WORKFLOW/MN7_RH_3_2_S13_220307_high_res/",
-    "ds_name": "high_res",
+    "outputDir": "/ACdata/processed/"+scope+"/"+dirname+"/",
+    "rootDir": rootDir + "/",
+    "ds_name": "",
     "mip_level": 3,
-    "md_filename": "acqinfo_metadata.json",
-    "consolidate_pos": True
+    "md_filename": rootDir+"/acqinfo_metadata.json",
+    "consolidate_pos": True,
+    "reverse_stitch": False,
+    "deskew": False
 }
 
 
@@ -34,6 +51,8 @@ class RunModulesSchema(argschema.ArgSchema):
                               description="Whether to consolidate all position"
                                           "strips in one neuroglancer layer or"
                                           "separate them into independent layers")
+    reverse_stitch = Boolean(required=False,default=False, description="Whether to reverse direction of stitching or not")
+    deskew = Boolean(required=False,default=False, description="Whether to deskew or not")
 
 
 class RunModules(argschema.ArgSchemaParser):
@@ -45,53 +64,57 @@ class RunModules(argschema.ArgSchemaParser):
                 "rootDir": run_input['rootDir'],
                 "fname": run_input['md_filename']
                 }
+        md = parse_metadata.ParseMetadata(input_data=md_input).get_md()
+        dsname = md["stripdirs"][0].split("_Pos")[0]
+        run_input["ds_name"]=dsname
+        if (md["positions"][0]["y_start_um"])>(md["positions"][1]["y_start_um"]):
+            run_input["reverse_stitch"] = True
+        print(run_input)
+        
+        deskew = 0
+        if run_input['deskew']:
+            deskew = np.cos(parse_metadata.ParseMetadata(input_data=md_input).get_angle())
+        
         n_pos = parse_metadata.ParseMetadata(input_data=md_input).get_number_of_positions()
         if n_pos == 0:
             print("No positions to process")
             return -1
 
-        for pos in range(n_pos):
-            convert_input = {
-                "ds_name": f"pos{pos}",
-                "max_mip": 4,
-                "concurrency": 20,
-                "input_dir": f"{run_input['rootDir']}/{run_input['ds_name']}_Pos{pos}",
-                "out_n5": f"{run_input['outputDir']}/Pos{pos}.n5"
-                }
-            multiscale_input = {
-                "position": pos,
-                "outputDir": f"{run_input['outputDir']}/Pos{pos}.n5",
-                'max_mip': 4,
-                "rootDir": f"{run_input['rootDir']}",
-                "fname": run_input['md_filename']
-                }
-            # Convert to n5 if not done already
-            if not os.path.isdir(convert_input['out_n5']):
-                tiff_to_n5.TiffDirToN5(input_data=convert_input).run()
-                # Add multiscale attributes
-                multiscale.Multiscale(input_data=multiscale_input).run()
-            else:
-                print(f"Skipping position {pos}, directory already exists")
-
-        # Create overview nglink, initialize state
-        state = {"showDefaultAnnotations": False, "layers": []}
-
-        if run_input["consolidate_pos"]:
-            layer_input = {
-                    "position": 0,
-                    "outputDir": run_input['outputDir'],
-                    "rootDir": run_input['rootDir']
-                    }
-            create_layer.NgLayer(input_data=layer_input).run_consolidate(state)
+        convert_input = {
+            "input_dir": f"{run_input['rootDir']}",
+            "output_dir": f"{run_input['outputDir']}",
+            "max_mip": 5,
+            "position_concurrency": 5         
+            }
+        if not os.path.isdir(convert_input['output_dir']):
+            acquisition_dir_to_n5_dir.AcquisitionDirToN5Dir(input_data=convert_input).run()
         else:
-            # Loop throuh each position to add a layer to state
-            for pos in range(n_pos):
+            print(f"Skipping conversion, {dirname} directory already exists")
+    
+        for pos in range(n_pos):
+        # Create overview nglink, initialize state
+            state = {"showDefaultAnnotations": False, "layers": []}
+
+            if run_input["consolidate_pos"]:
                 layer_input = {
-                    "position": pos,
-                    "outputDir": run_input['outputDir'],
-                    "rootDir": run_input['rootDir']
-                    }
-                create_layer.NgLayer(input_data=layer_input).run(state)
+                        "position": 0,
+                        "outputDir": run_input['outputDir']+dirname+".n5/",
+                        "rootDir": run_input['rootDir'],
+                        "reverse": run_input["reverse_stitch"],
+                        "deskew": deskew
+                        }
+                create_layer.NgLayer(input_data=layer_input).run_consolidate(state)
+            else:
+                # Loop throuh each position to add a layer to state
+                for pos in range(n_pos):
+                    layer_input = {
+                        "position": pos,
+                        "outputDir": run_input['outputDir']+dirname+".n5/",
+                        "rootDir": run_input['rootDir'],
+                        "reverse": run_input["reverse_stitch"],
+                        "deskew": deskew
+                        }
+                    create_layer.NgLayer(input_data=layer_input).run(state)
 
         # Create nglink from created state and estimated positions (overview link)
         nglink_input = {
@@ -107,7 +130,9 @@ class RunModules(argschema.ArgSchemaParser):
         create_json_input = {
                 'rootDir': run_input['rootDir'],
                 'outputDir': run_input['outputDir'],
-                "mip_level": run_input['mip_level']
+                "mip_level": run_input['mip_level'],
+                "reverse": run_input['reverse_stitch'],
+                "dirname": dirname
                 }
         create_json.CreateJson(input_data=create_json_input).run()
 
