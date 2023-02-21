a83311801aaa74044642d9019f219af474c6e594
Kevin Takasaki <kevint@alleninstitute.org>
2023-02-21 09:07:40 -0800

    removing block print out

--- acpreprocessing/stitching_modules/convert_to_n5/run_tiff_to_zarr.py
+++ acpreprocessing/stitching_modules/convert_to_n5/run_tiff_to_zarr.py
@@ -10,6 +10,6 @@ from tiff_to_zarr import tiffdir_to_n5_group
 for i in range(0,39):
     gstr = 'highres_Pos' + str(i)
     tiffdir = 'Y:/workflow_data/iSPIM2/MNx_S3bB_230215_highres/' + gstr
-    outzarr = 'H:/highres_crop_zstd/' + gstr
+    outzarr = 'H:/highres_crop/' + gstr
     grpname = gstr
     tiffdir_to_n5_group(tiffdir,output_n5=outzarr,group_names=[grpname],concurrency=20,max_mip=5,deskew_str='ispim2')
