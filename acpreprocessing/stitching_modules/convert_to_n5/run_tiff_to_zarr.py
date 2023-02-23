8c2999f26bd95c71623900f83ef2ee1e15a4ed70
Kevin Takasaki <kevint@alleninstitute.org>
2023-02-23 09:16:50 -0800

    updated run script to ngff

--- acpreprocessing/stitching_modules/convert_to_n5/run_tiff_to_zarr.py
+++ acpreprocessing/stitching_modules/convert_to_n5/run_tiff_to_zarr.py
@@ -5,11 +5,11 @@ Created on Fri Feb 17 19:42:51 2023
 @author: kevint
 """
 
-from tiff_to_zarr import tiffdir_to_n5_group
+from tiff_to_ngff import tiffdir_to_ngff_group
 
-for i in range(0,39):
+for i in range(0,41):
     gstr = 'highres_Pos' + str(i)
-    tiffdir = 'Y:/workflow_data/iSPIM2/MNx_S3bB_230215_highres/' + gstr
-    outzarr = 'H:/highres_crop/' + gstr
+    tiffdir = 'Y:/workflow_data/iSPIM2/MNx_S3AG_230222_highres/' + gstr
+    outzarr = 'H:/MNx_S3AG_230222_highres_crop/' + gstr
     grpname = gstr
-    tiffdir_to_n5_group(tiffdir,output_n5=outzarr,group_names=[grpname],concurrency=20,max_mip=5,deskew_str='ispim2')
+    tiffdir_to_ngff_group(tiffdir,output_n5=outzarr,group_names=[grpname],output='zarr',concurrency=20,max_mip=5,deskew_str='ispim2')
