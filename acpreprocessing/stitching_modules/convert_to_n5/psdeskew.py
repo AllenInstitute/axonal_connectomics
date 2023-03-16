ce0d203e9b25ab53e1194d48753d96854fc25896
Kevin Takasaki <kevint@alleninstitute.org>
2023-03-16 14:37:56 -0700

    Revert "working on argschema"
    This reverts commit b29f9d1cdf681ea94ec37b0cb8db6b281b598ec8.

--- acpreprocessing/stitching_modules/convert_to_n5/psdeskew.py
+++ acpreprocessing/stitching_modules/convert_to_n5/psdeskew.py
@@ -10,10 +10,9 @@ NOTE: must be run sequentially as each tiff chunk contains data for the next des
 
 import numpy as np
 
-
-def psdeskew_kwargs(skew_dims_zyx, stride=1, deskewFlip=False, dtype='uint16', crop_factor=1, **kwargs):
+def psdeskew_kwargs(skew_dims_zyx,stride=1,deskewFlip=False,dtype='uint16',crop_factor=1,**kwargs):
     """get keyword arguments for deskew_block
-
+    
     Parameters
     ----------
     skew_dims_zyx : tuple of int
@@ -26,16 +25,16 @@ def psdeskew_kwargs(skew_dims_zyx, stride=1, deskewFlip=False, dtype='uint16', c
         datatype for deskew output
     crop_factor : float
         reduce y dimension according to ydim*crop_factor < ydim
-
+    
     Returns
     ----------
     dict of parameters representing pixel deskew operation for deskew_block
     """
     sdims = skew_dims_zyx
     ydim = int(sdims[1]*crop_factor)
-    blockdims = (int(sdims[2]/stride), ydim, stride*sdims[0])
+    blockdims = (int(sdims[2]/stride),ydim,stride*sdims[0])
     subblocks = int(np.ceil((sdims[2]+stride*sdims[0])/(stride*sdims[0])))
-    # print(subblocks)
+    #print(subblocks)
     blockx = sdims[0]
     dsi = []
     si = []
@@ -49,31 +48,29 @@ def psdeskew_kwargs(skew_dims_zyx, stride=1, deskewFlip=False, dtype='uint16', c
                 sxstart = 0
             if sxend > sdims[2]:
                 sxend = sdims[2]
-            sx = np.arange(sxstart, sxend)
+            sx = np.arange(sxstart,sxend)
             sxv.append(sx)
-            szv.append(sz*np.ones(sx.shape, dtype=sx.dtype))
+            szv.append(sz*np.ones(sx.shape,dtype=sx.dtype))
         sxv = np.concatenate(sxv)
         szv = np.concatenate(szv)
         dsx = sxv + stride*szv - i_block*stride*blockx
         dsz = np.floor(sxv/stride).astype(int)
-        dsi.append(np.ravel_multi_index(
-            (dsz, dsx), (blockdims[0], blockdims[2])))
-        si.append(np.ravel_multi_index((szv, sxv), (sdims[0], sdims[2])))
-    kwargs = {'dsi': dsi,
-              'si': si,
-              'slice1d': np.zeros((subblocks, blockdims[1], blockdims[2]*blockdims[0]), dtype=dtype),
-              'blockdims': blockdims,
-              'subblocks': subblocks,
-              'flip': deskewFlip,
-              'dtype': dtype,
+        dsi.append(np.ravel_multi_index((dsz,dsx),(blockdims[0],blockdims[2])))
+        si.append(np.ravel_multi_index((szv,sxv),(sdims[0],sdims[2])))
+    kwargs = {'dsi' : dsi,
+              'si' : si,
+              'slice1d' : np.zeros((subblocks,blockdims[1],blockdims[2]*blockdims[0]),dtype=dtype),
+              'blockdims' : blockdims,
+              'subblocks' : subblocks,
+              'flip' : deskewFlip,
+              'dtype' : dtype,
               'chunklength': blockx
               }
     return kwargs
-
-
-def deskew_block(blockData, n, dsi, si, slice1d, blockdims, subblocks, flip, dtype, chunklength, *args, **kwargs):
+        
+def deskew_block(blockData,n,dsi,si,slice1d,blockdims,subblocks,flip,dtype,chunklength,*args,**kwargs):
     """deskew a data chunk in sequence with prior chunks
-
+    
     Parameters
     ----------
     blockData : numpy.ndarray
@@ -96,13 +93,13 @@ def deskew_block(blockData, n, dsi, si, slice1d, blockdims, subblocks, flip, dty
         datatype
     chunklength : int
         number of slices expected for raw data block (for zero filling)
-
+    
     Returns
     ----------
     ndarray of pixel shifted deskewed data ordered (z,y,x) by sample axes 
     """
     subb = subblocks
-    block3d = np.zeros(blockdims, dtype=dtype)
+    block3d = np.zeros(blockdims,dtype=dtype)
     zdim = block3d.shape[0]
     ydim = block3d.shape[1]
     xdim = block3d.shape[2]
@@ -110,38 +107,36 @@ def deskew_block(blockData, n, dsi, si, slice1d, blockdims, subblocks, flip, dty
     if blockData.shape[1] > ydim:
         y0 = int(np.floor((blockData.shape[1]-ydim)/2))
         y1 = int(np.floor((blockData.shape[1]+ydim)/2))
-        blockData = blockData[:, y0:y1, :]
+        blockData = blockData[:,y0:y1,:]
     #print('deskewing block ' + str(n) + ' with shape ' + str(blockData.shape))
     if blockData.shape[0] < chunklength:
         #print('block is short, filling with zeros')
-        blockData = np.concatenate((blockData, np.zeros(
-            (int(chunklength-blockData.shape[0]), blockData.shape[1], blockData.shape[2]))))
-    order = (np.arange(subb)+n) % subb
+        blockData = np.concatenate((blockData,np.zeros((int(chunklength-blockData.shape[0]),blockData.shape[1],blockData.shape[2]))))
+    order = (np.arange(subb)+n)%subb
     for y in range(ydim):
-        for i, o in enumerate(order):
+        for i,o in enumerate(order):
             # flip stack axis 2 for ispim2
             s = -1 if flip else 1
-            slice1d[o, y, :][dsi[i]] = blockData[:, y, ::s].ravel()[si[i]]
-        block3d[:, y, :] = slice1d[n % subb, y, :].reshape((zdim, xdim))
-        slice1d[n % subb, y, :] = 0
+            slice1d[o,y,:][dsi[i]] = blockData[:,y,::s].ravel()[si[i]]
+        block3d[:,y,:] = slice1d[n%subb,y,:].reshape((zdim,xdim))
+        slice1d[n%subb,y,:]=0
     return block3d
-
-
-def reshape_joined_shapes(joined_shapes, stride, blockdims, *args, **kwargs):
+    
+def reshape_joined_shapes(joined_shapes,stride,blockdims,*args,**kwargs):
     deskewed_shape = (int(np.ceil(joined_shapes[0]/(blockdims[2]/stride))*blockdims[2]),
-                      blockdims[1],
-                      blockdims[0])
+                                   blockdims[1],
+                                   blockdims[0])
     return deskewed_shape
 
-# def options_from_str(idstr):
-#     """config lookup for deskew options by keystring
-#     """
-#     if idstr == 'ispim2':
-#         options = {'stride':2,
-#                    'deskewFlip':True,
-#                    'dtype':'uint16',
-#                    'crop_factor':0.5}
-#     else:
-#         options = {}
-
-#     return options
+def options_from_str(idstr):
+    """config lookup for deskew options by keystring
+    """
+    if idstr == 'ispim2':
+        options = {'stride':2,
+                   'deskewFlip':True,
+                   'dtype':'uint16',
+                   'crop_factor':0.5}
+    else:
+        options = {}
+    
+    return options
