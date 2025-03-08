e77785ea62e95c4352c760afe5e5a9ab1768265f
Kevin Takasaki <kevint@alleninstitute.org>
2025-03-07 22:21:52 -0800

    hack with z5py

--- acpreprocessing/stitching_modules/convert_to_n5/zarrv3_to_ngff.py
+++ acpreprocessing/stitching_modules/convert_to_n5/zarrv3_to_ngff.py
@@ -522,7 +522,9 @@ class Zarrv3ToZarrInputParameters(argschema.ArgSchema,
     chunk_size = argschema.fields.Tuple((
         argschema.fields.Int(),
         argschema.fields.Int(),
-        argschema.fields.Int()), required=False, default=(64, 64, 64))
+        argschema.fields.Int(),
+        argschema.fields.Int(),
+        argschema.fields.Int()), required=False, default=(1, 1, 64, 64, 64))
 
 
 
